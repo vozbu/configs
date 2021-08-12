45fb8ec7603bdd0b77cd5562c85bc9470d59d5db
Valeriy Bykov <valery.bickov@gmail.com>
2021-08-12 20:45:14 +0300

    [+] fish: jobs count

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -39,8 +39,13 @@ function fish_prompt --description 'Write out the prompt'
     set -q fish_prompt_pwd_dir_length
     or set -lx fish_prompt_pwd_dir_length 0
 
+    set j (jobs | grep -v 'no jobs' | wc -l)
+
     set_color -b black
     printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (prompt_pwd) (set_color white) $git_info (set_color white) '❱' (set_color white)
+    if test $j -gt 0
+        printf '%s%d%s' (set_color red) $j (set_color normal)
+    end
     if test $laststatus -eq 0
         printf "%s✔%s≻%s " (set_color -o green) (set_color white) (set_color normal)
     else
