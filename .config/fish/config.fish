a26ab80080bb1a4eef7fcf7e1401cdc59d54e486
Valeriy Bykov <valery.bickov@gmail.com>
2021-08-26 15:41:35 +0300

    [*] fish: fixed exported variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ set CCACHE_SLOPPINESS pch_defines,time_macros
 set EDITOR "/usr/bin/vim"
 set MANPAGER "env MAN_PN=1 vim -M +MANPAGER -"
 set MANPATH "$HOME/.local/share/man" "$MANPATH"
-set GIT_DISCOVERY_ACROSS_FILESYSTEM 1
+set -gx GIT_DISCOVERY_ACROSS_FILESYSTEM 1
 set MAKEFLAGS "-j "(nproc)
 
 alias dockviz="docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz"
