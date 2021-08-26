b84d7097c1aad7ba0221023672870ec3f2dabebf
Valeriy Bykov <valery.bickov@gmail.com>
2021-08-26 15:41:25 +0300

    [!] fish: more familiar git prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,47 +2,33 @@ function fish_prompt --description 'Write out the prompt'
     set laststatus $status
 
     if set -l git_branch (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
-        #set git_branch (set_color -o blue)"$git_branch"
 
         set -l upstream (git rev-parse --abbrev-ref --symbolic-full-name @{u} 2> /dev/null)
+
         if set -l count (command git rev-list --count --left-right $upstream...HEAD 2>/dev/null)
             echo $count | read -l behind ahead
-            if test "$ahead" -gt 0
-                set git_status "$git_status"(set_color red)⬆
-            end
             if test "$behind" -gt 0
-                set git_status "$git_status"(set_color red)⬇
+                set git_status "$git_status"(set_color yellow)"<<"
+            end
+            if test "$ahead" -gt 0
+                set git_status "$git_status"(set_color yellow)">>"
             end
         end
 
-        if not command git diff-index --quiet HEAD --
-            set git_branch_line (set_color red)"$git_branch"(set_color white)
+        set LINES (LANG=C git status -sb 2>/dev/null | wc -l)
 
-            for i in (git status --porcelain | string sub -l 2 | uniq)
-                switch $i
-                    case "."
-                        set git_status "$git_status"(set_color green)✚
-                    case " D"
-                        set git_status "$git_status"(set_color red)✖
-                    case "*M*"
-                        set git_status "$git_status"(set_color green)✱
-                    case "*R*"
-                        set git_status "$git_status"(set_color purple)➜
-                    case "*U*"
-                        set git_status "$git_status"(set_color brown)═
-                    case "??"
-                        set git_status "$git_status"(set_color red)≠
-                end
-            end
+        #if not command git diff-index --quiet HEAD --
+        if test $LINES -gt 1
+            set git_branch_line (set_color red)"$git_branch"(set_color white)
         else
             set git_branch_line (set_color green)"$git_branch"(set_color white)
         end
 
-        if not set -q git_status
-            set git_status (set_color green):
+        if set -q git_status
+            set git_status " $git_status"
         end
 
-        set git_info "(git: $git_branch_line $git_status"(set_color white)")"
+        set git_info "(git: $git_branch_line$git_status"(set_color white)")"
     end
 
     # Disable PWD shortening by default.
@@ -54,7 +40,7 @@ function fish_prompt --description 'Write out the prompt'
     set_color -b black
     printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (prompt_pwd) (set_color white) $git_info (set_color white) '❱' (set_color white)
     if test $j -gt 0
-        printf '%s%d%s' (set_color red) $j (set_color normal)
+        printf '%s %d%s' (set_color yellow) $j (set_color normal)
     end
     if test $laststatus -eq 0
         printf "%s✔%s≻%s " (set_color -o green) (set_color white) (set_color normal)
