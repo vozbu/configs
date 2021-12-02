f1fc376b8735b6e51862bf1dacc7d8d790307ba4
Valeriy Bykov <valery.bickov@gmail.com>
2021-12-02 21:32:00 +0300

    [*] fish: global export of al variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,13 @@
-set PATH /home/vozbu/.local/bin /home/vozbu/.cargo/bin /home/vozbu/programming/go/bin /home/vozbu/bin $PATH
-set GOPATH "$HOME/programming/go"
-set GOFLAGS "-mod=vendor"   # https://github.com/golang/go/issues/41301
-set CCACHE_COMPRESS 1
-set CCACHE_SLOPPINESS pch_defines,time_macros
-set EDITOR "/usr/bin/vim"
-set MANPAGER "env MAN_PN=1 vim -M +MANPAGER -"
-set MANPATH "$HOME/.local/share/man" "$MANPATH"
+set -gx PATH /home/vozbu/.local/bin /home/vozbu/.cargo/bin /home/vozbu/programming/go/bin /home/vozbu/bin $PATH
+set -gx GOPATH "$HOME/programming/go"
+set -gx GOFLAGS "-mod=vendor"   # https://github.com/golang/go/issues/41301
+set -gx CCACHE_COMPRESS 1
+set -gx CCACHE_SLOPPINESS pch_defines,time_macros
+set -gx EDITOR "/usr/bin/vim"
+set -gx MANPAGER "env MAN_PN=1 vim -M +MANPAGER -"
+set -gx MANPATH "$HOME/.local/share/man" "$MANPATH"
 set -gx GIT_DISCOVERY_ACROSS_FILESYSTEM 1
-set MAKEFLAGS "-j "(nproc)
+set -gx MAKEFLAGS "-j "(nproc)
 set -gx TESSDATA_PREFIX /usr/share/tessdata
 
 alias dockviz="docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz"
