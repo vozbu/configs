0235b1a9ed86a8662fc8f4f07feaedbe6b565c1e
Valeriy Bykov <valery.bickov@gmail.com>
2025-02-18 22:45:19 +0300

    [!] plugin for using vim as man pager and correspnding setting in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ set -gx CCACHE_COMPRESS 1
 set -gx CCACHE_SLOPPINESS pch_defines,time_macros
 set -gx EDITOR "/usr/bin/vim"
 set -gx MANPAGER "env MAN_PN=1 vim -M +MANPAGER -"
+set -gx MANPAGER "vim -c ASMANPAGER -"
 set -gx MANPATH "$HOME/.local/share/man" "$MANPATH"
 set -gx GIT_DISCOVERY_ACROSS_FILESYSTEM 1
 set -gx MAKEFLAGS "-j "(nproc)
